4a48c5d3bedcc2cf5c597d61634312600f36a407
byrandrs <byrandrs@gmail.com>
2020-03-11 19:53:04 -0500

    Mejoras 1.0

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $("#show").mouseover(function() {
+        $("#menunav").show()
+   })
+    $("#show").mouseout(function() {
+        $("#menunav").hide()
+    })
+
+})
+ 	
+ 
+    //$("#show").on('click', function() {
+    //    $("#element").show();
+    //    return false;
+    //});
+   
